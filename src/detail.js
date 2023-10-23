fcb0abcd3edf6f29f5de2683fa981f8267b6f9e0
kimmingyo8 <kimmingyo8@naver.com>
2023-10-23 23:31:28 +0900

    #3 - 데이터를 가져와 detail페이지에 뿌려주는 작업

---
+++ src/detail.js
@@ -0,0 +1,44 @@
+import zodiacData from '../assets/zodiacFeature.js';
+
+const urlParams = new URLSearchParams(window.location.search);
+const zodiacName = urlParams.get('zodiac');
+
+const data = zodiacData[zodiacName];
+
+const $h1 = document.querySelector('h1');
+const $emoji = document.querySelector('.info__emoji');
+const $krName = document.querySelector('.info__kr');
+const $engName = document.querySelector('.info__eng');
+const $date = document.querySelector('.info__day');
+const $featureList = document.querySelectorAll('.content__list');
+
+const $positiveList = $featureList[0];
+const $negativeList = $featureList[1];
+
+const { start, end } = data;
+const range = dateRangeFormat(start, end);
+const positiveTraits = data.traits.positive;
+const negativeTraits = data.traits.negative;
+
+function dateRangeFormat(start, end) {
+  const range = `${start} ~ ${end}`;
+  return range;
+}
+
+positiveTraits.forEach((trait) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = `❤️  ${trait}`;
+  $positiveList.appendChild(listItem);
+});
+
+negativeTraits.forEach((trait) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = `🥲 ${trait}...`;
+  $negativeList.appendChild(listItem);
+});
+
+$h1.innerText = zodiacName;
+$emoji.innerText = data.emoji;
+$krName.innerText = data.kr;
+$engName.innerText = data.name;
+$date.innerText = range;
